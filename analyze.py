3c7e021937d66f868663efc098479bb04765b4b8
Mihai Diac <mihai.diac@metro.digital>
2021-11-23 20:00:27 +0200

    Add avg and count to analyze

--- analyze.py
+++ analyze.py
@@ -1,5 +1,7 @@
 import csv
 from datetime import datetime
+from pprint import pprint
+from decimal import Decimal
 
 summary = {};
 
@@ -7,8 +9,20 @@ with open('trades_finished.csv') as csvfile:
     for row in csv.reader(csvfile):
         date = datetime.strptime(row[7], '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
         if date in summary:
-        	summary[date] = summary[date] + float(row[6])
+        	summary[date] = {
+                'pos_count' : summary[date]['pos_count'] + 1 if float(row[6]) > 0 else summary[date]['pos_count'],
+                'neg_count' : summary[date]['neg_count'] + 1 if float(row[6]) < 0 else summary[date]['neg_count'],
+                'sum' : summary[date]['sum'] + float(row[6]),
+            }
         else:
-        	summary[date] = float(row[6])
+        	summary[date] = {
+                'pos_count' : 1 if float(row[6]) > 0 else 0,
+                'neg_count' : 1 if float(row[6]) < 0 else 0,
+                'sum' : float(row[6]),
+            }
 
-print(summary)
+for item in summary:
+    # print(summary[item])
+    summary[item]['avg'] = summary[item]['sum'] / (summary[item]['pos_count'] + summary[item]['neg_count'])
+
+pprint(summary)
