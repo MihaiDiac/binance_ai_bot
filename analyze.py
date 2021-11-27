2986be92b4c2c3f32638fcd3dec9cdf0ab7aad9f
Mihai Diac <mihai.diac@metro.digital>
2021-11-27 11:39:01 +0200

    Reorganize analyzed stats

--- analyze.py
+++ analyze.py
@@ -3,35 +3,44 @@ from datetime import datetime
 from pprint import pprint
 import os
 
-def get_summary(trades_file):
-    summary = {};
-
+def get_summary(summary, branch, trades_file):
     with open(trades_file, mode='r') as csvfile:
         for row in csv.reader(csvfile):
             date = datetime.strptime(row[7], '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
-            if date in summary:
-            	summary[date] = {
-                    'pos_count' : summary[date]['pos_count'] + 1 if float(row[6]) > 0 else summary[date]['pos_count'],
-                    'neg_count' : summary[date]['neg_count'] + 1 if float(row[6]) <= 0 else summary[date]['neg_count'],
-                    'sum' : summary[date]['sum'] + float(row[6]),
-                }
-            else:
-            	summary[date] = {
+            if not date in summary:
+                summary[date] = {}
+                summary[date][branch] = {
                     'pos_count' : 1 if float(row[6]) > 0 else 0,
                     'neg_count' : 1 if float(row[6]) < 0 else 0,
                     'sum' : float(row[6]),
                 }
+            else:
+                if not branch in summary[date]:
+                    summary[date][branch] = {
+                        'pos_count' : 1 if float(row[6]) > 0 else 0,
+                        'neg_count' : 1 if float(row[6]) < 0 else 0,
+                        'sum' : float(row[6]),
+                    }
+                else:
+                    summary[date][branch] = {
+                        'pos_count' : summary[date][branch]['pos_count'] + 1 if float(row[6]) > 0 else summary[date][branch]['pos_count'],
+                        'neg_count' : summary[date][branch]['neg_count'] + 1 if float(row[6]) <= 0 else summary[date][branch]['neg_count'],
+                        'sum' : summary[date][branch]['sum'] + float(row[6])
+                    }
 
     for item in summary:
-        summary[item]['avg'] = summary[item]['sum'] / (summary[item]['pos_count'] + summary[item]['neg_count'])
-        summary[item]['ratio'] = 100 * (summary[item]['pos_count'] / summary[item]['neg_count'] - 1)
+        if branch in summary[item]:
+            summary[item][branch]['avg'] = summary[item][branch]['sum'] / (summary[item][branch]['pos_count'] + summary[item][branch]['neg_count'])
+            summary[item][branch]['ratio'] = 100 * (summary[item][branch]['pos_count'] / summary[item][branch]['neg_count'] - 1)
 
     return summary
 
+summary = {}
+
 if os.path.exists('trades_finished.csv'):
-    summary = get_summary('trades_finished.csv')
-    pprint(summary)
+    summary = get_summary(summary, 'predicted', 'trades_finished.csv')
 
 if os.path.exists('trades_finished_all.csv'):
-    summary = get_summary('trades_finished_all.csv')
-    pprint(summary)
+    summary = get_summary(summary, 'all', 'trades_finished_all.csv')
+
+pprint(summary)
