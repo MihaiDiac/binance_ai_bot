f571099091cb3eb65b7d4b84cf627bb6e0c8f27a
Mihai Diac <mihai.diac@metro.digital>
2021-11-28 11:32:23 +0200

    Error handling

--- predict.py
+++ predict.py
@@ -11,13 +11,13 @@ import os
 tf.config.set_visible_devices([], 'GPU')
 
 def get_model():
-    return tf.keras.models.load_model('model')
+    return tf.keras.models.load_model('model') if os.path.exists('model/saved_model.pb') else None
 
 def predict(model, previous_stats, current_stats):
     if (not previous_stats):
         exit()
     
-    predict_data = numpy.empty(shape=[0, 3])
+    predict_data = numpy.empty(shape=[0, 2])
 
     for current_stat in current_stats:
         previous_stat = [d for d in previous_stats if d['symbol'] == current_stat['symbol']]
@@ -27,9 +27,9 @@ def predict(model, previous_stats, current_stats):
 
         delta_price = 100 * (current_stat['price'] - previous_stat[0]['price']) / previous_stat[0]['price']
         delta_volume = 100 * (current_stat['volume'] - previous_stat[0]['volume']) / previous_stat[0]['volume']
-        predict_data = numpy.append(predict_data, [[delta_price, delta_volume, current_stat['price']]], axis = 0)
+        predict_data = numpy.append(predict_data, [[delta_price, delta_volume]], axis = 0)
 
-    predictions = model.predict(predict_data)
+    predictions = model.predict(predict_data) if model != None else None
 
     predicted_data = []
     for i, current_stat in enumerate(current_stats):
@@ -44,13 +44,18 @@ def predict(model, previous_stats, current_stats):
             'symbol' : current_stat['symbol'], 
             'delta_price' : delta_price,
             'delta_volume' : delta_volume,
-            'prediction' : predictions[i][0],
+            'prediction' : predictions[i][0] if predictions != None else 0,
             'buy_price' : current_stat['price'],
             'buy_time' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         })
 
     sorted(predicted_data, key = itemgetter('prediction'), reverse = True)
-    return predicted_data[:15]
+
+    return {
+        'all': predicted_data, 
+        'predicted': predicted_data[:15] if predictions != None else [random.choice(predicted_data) for i in range(15)],
+        'random': [random.choice(predicted_data) for i in range(15)]
+    }
 
 def get_stats():
     stats = []
@@ -75,28 +80,32 @@ def set_stats():
     return stats
 
 
-def buy(items):
-    for item in items:
-        if (item['delta_price'] != 0 and item['delta_volume'] != 0):
-            with open('trades_active.csv', mode='a', newline='') as csvfile:
-                writer = csv.writer(csvfile)
-                writer.writerow([item['symbol'], item['delta_price'], item['delta_volume'], item['prediction'], item['buy_price'], item['buy_time']])
+def buy(predicted_data):
+    for category in predicted_data:
+        for coin in predicted_data[category]:
+            if (coin['delta_price'] != 0 and coin['delta_volume'] != 0):
+                with open('trades_active_' + category + '.csv', mode='a', newline='') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow([coin['symbol'], coin['delta_price'], coin['delta_volume'], coin['prediction'], coin['buy_price'], coin['buy_time']])
 
 def sell(current_stats):
-    if os.path.exists('trades_active.csv'):
-        with open('trades_active.csv', 'r+') as active, open('trades_finished.csv', 'a', newline='') as finished:
-            writer = csv.writer(finished)
-            for row in csv.reader(active):
-                sell_price = [d for d in current_stats if d['symbol'] == row[0]][0]['price']
-                profit = 100 * (sell_price / float(row[4]) - 1)
-                writer.writerow([row[0], row[1], row[2], row[3], row[4], sell_price, profit, row[5], datetime.now().strftime('%Y-%m-%d %H:%M:%S')])
-            active.truncate(0)
+    categories = ['all', 'predicted', 'random']
+
+    for category in categories:
+        if os.path.exists('trades_active_' + category + '.csv'):
+            with open('trades_active_' + category + '.csv', 'r+') as active, open('trades_finished_' + category + '.csv', 'a', newline='') as finished:
+                writer = csv.writer(finished)
+                for row in csv.reader(active):
+                    sell_price = [d for d in current_stats if d['symbol'] == row[0]][0]['price']
+                    profit = 100 * (sell_price / float(row[4]) - 1)
+                    writer.writerow([row[0], row[1], row[2], row[3], row[4], sell_price, profit, row[5], datetime.now().strftime('%Y-%m-%d %H:%M:%S')])
+                active.truncate(0)
     
 
 def main():
     previous_stats = get_stats() if os.path.exists('stats.csv') else None
     current_stats = set_stats()
-    
+
     sell(current_stats)
     buy(predict(get_model(), previous_stats, current_stats))
 
--- train.py
+++ train.py
@@ -1,24 +1,28 @@
 import tensorflow as tf
 import numpy
 import csv
+import os
 
 tf.config.set_visible_devices([], 'GPU')
 
 def train():
+    if (not os.path.exists('trades_finished_all.csv')):
+        exit('No data for model training!')
+
     model = tf.keras.Sequential([
-        tf.keras.layers.Dense(input_shape = (3,), units = 20, activation = tf.nn.relu),
+        tf.keras.layers.Dense(input_shape = (2,), units = 20, activation = tf.nn.relu),
     	tf.keras.layers.Dense(units = 20, activation = tf.nn.relu),
         tf.keras.layers.Dense(units = 1)
     ])
 
     model.compile(optimizer='adam', loss='mse', metrics=['mae'])
 
-    train_data = numpy.empty(shape=[0, 3]);
+    train_data = numpy.empty(shape=[0, 2]);
     train_targets = numpy.empty(shape=[0, 1]);
 
     with open('trades_finished_all.csv') as csvfile:    
         for row in csv.reader(csvfile):
-            train_data = numpy.append(train_data, [[float(row[1]), float(row[2]), float(row[4])]], axis = 0)
+            train_data = numpy.append(train_data, [[float(row[1]), float(row[2])]], axis = 0)
             train_targets = numpy.append(train_targets, [float(row[5])])
 
     model.fit(train_data, train_targets, epochs=100, batch_size=1)
