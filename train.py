be8356da723d7064391e836b97ba115c7327582b
Mihai Diac <mihai.diac@metro.digital>
2021-12-05 12:59:09 +0200

    Replace csv files with binance historical data

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import numpy
 import csv
 import os
+from binance.client import Client
 from configparser import ConfigParser
 
 config = ConfigParser()
@@ -11,26 +12,31 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = config['tensorflow']['log_level']
 import tensorflow as tf
 
 def train():
-    if (not os.path.exists('trades_finished_all.csv')):
-        exit('Error: no data for model training!')
-
     model = tf.keras.Sequential([
-        tf.keras.layers.Dense(input_shape = (2,), units = 20, activation = tf.nn.relu),
+        tf.keras.layers.Dense(input_shape = (4,), units = 20, activation = tf.nn.relu),
     	tf.keras.layers.Dense(units = 20, activation = tf.nn.relu),
         tf.keras.layers.Dense(units = 1)
     ])
 
     model.compile(optimizer='adam', loss='mse', metrics=['mae'])
 
-    train_data = numpy.empty(shape=[0, 2]);
-    train_targets = numpy.empty(shape=[0, 1]);
+    klines = Client().get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1HOUR, "01 Jan 2017")
 
-    with open('trades_finished_all.csv') as csvfile:    
-        for row in csv.reader(csvfile):
-            train_data = numpy.append(train_data, [[float(row[1]), float(row[2])]], axis = 0)
-            train_targets = numpy.append(train_targets, [float(row[5])])
+    x_train = numpy.empty(shape=[0, 4]);
+    y_train = numpy.empty(shape=[0, 1]);
+    ath = 0
 
-    model.fit(train_data, train_targets, epochs=100, batch_size=1)
+    for i, kline in enumerate(klines):
+        if (i == 0):
+            x_train = numpy.append(x_train, [[float(kline[1]), float(kline[2]), float(kline[3]), float(kline[4])]], axis = 0)
+        elif (i == len(klines) - 1):
+            y_train = numpy.append(y_train, [[float(klines[i][4])]], axis = 0)
+        else:
+            x_train = numpy.append(x_train, [[float(kline[1]), float(kline[2]), float(kline[3]), float(kline[4])]], axis = 0)
+            y_train = numpy.append(y_train, [[float(klines[i][4])]], axis = 0)
+        ath = float(klines[i][2]) if float(klines[i][2]) > ath else ath
+
+    model.fit(x_train / ath, y_train / ath, epochs=100, batch_size=1)
 
     return model
 
